f10362a0daa3a34bcc37736a283da0146299ab94
Divyaant Kumar Jain <73459836+Divyaantkj01@users.noreply.github.com>
2022-10-12 00:31:23 +0530

    Added display() function

--- main.cpp
+++ main.cpp
@@ -33,6 +33,48 @@ void C_node()
         temp = newnode;
     }
 }
+//Sorting and display in phonebook
+
+
+
+void display() //display function
+
+{
+
+	int len=0;
+
+	if (head==NULL)
+
+	cout<<"Contact list is Empty!"<<endl;
+
+	else
+
+	{
+
+		bubblesort();
+
+		node *trav=head;
+
+		while(trav!=NULL)
+
+		{
+
+			cout<<"\n\tFull Name"<<trav->name<<endl;
+
+			cout<<"\t\tPhone number"<<trav->number<<endl;
+
+			trav=trav->next;
+
+			len++;
+
+		}
+
+		cout<<"Total contacts in the list:"<<len<<endl;
+
+	}
+
+}
+
 
 void search_contact()
 {
