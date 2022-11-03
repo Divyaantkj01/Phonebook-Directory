ad26e73e164ab60eb946b947431d395a2e8b9bec
Divyaant <divyaant.209301332@muj.manipal.edu>
2022-11-03 09:17:46 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ void C_node()
 
     cout << " Enter number ";
     cin >> newnode->number;
-    cout<<"\n";
+    cout << "\n";
 
     newnode->next = NULL;
 
@@ -36,7 +36,7 @@ void C_node()
 }
 void display()
 {
-    
+
     if (head == NULL)
     {
         cout << " Contact list is Empty " << endl;
@@ -48,12 +48,12 @@ void display()
         {
             cout << "\n\tFull Name: " << temp->name << endl;
             cout << "\tPhone Number: " << temp->number << endl;
-            cout<<"\n";
+            cout << "\n";
             temp = temp->next;
             len++;
         }
         cout << "Total contacts in the list = " << len << endl;
-        cout<<"\n";
+        cout << "\n";
     }
 }
 
@@ -89,7 +89,7 @@ void search_contact()
     {
 
         cout << "\tIndex of Contact = " << count << endl;
-        cout<<"\n";
+        cout << "\n";
     }
     else
     {
@@ -97,52 +97,166 @@ void search_contact()
     }
 }
 
-
-void at_given(){
+void at_given()
+{
     int pos;
     node *next_node;
     temp = head;
-    cout<<"Enter your desired position from where you want to delete contact"<<endl;
-    cin>>pos;
+    cout << "Enter your desired position from where you want to delete contact" << endl;
+    cin >> pos;
 
-    if(head == NULL){
-        cout<<"List is empty"<<endl;
+    if (head == NULL)
+    {
+        cout << "List is empty" << endl;
     }
-    else if(pos > len){
-        cout<<"Invalid Position"<<endl;
+    else if (pos > len)
+    {
+        cout << "Invalid Position" << endl;
     }
 
-    else if(pos == 0){
+    else if (pos == 0)
+    {
         temp = head;
         head = head->next;
         delete temp;
-        cout<<"Contact has been deleted"<<endl;
+        cout << "Contact has been deleted" << endl;
     }
-    else{
-        for(int i = 1;i<pos;i++){
-            temp = temp->next;   
+    else
+    {
+        for (int i = 1; i < pos; i++)
+        {
+            temp = temp->next;
         }
         next_node = temp->next;
         temp->next = next_node->next;
         delete next_node;
-        cout<<"Contact has been deleted"<<endl;
+        cout << "Contact has been deleted" << endl;
     }
 }
 
-void clear_all(){
-    if(head == NULL){
-        cout<<"List is Empty"<<endl;
+void clear_all()
+{
+    if (head == NULL)
+    {
+        cout << "List is Empty" << endl;
     }
-    else{
+    else
+    {
         temp = head;
-        while(head != NULL){
+        while (head != NULL)
+        {
             head = head->next;
             delete temp;
         }
-        cout<<"All Contact list has been deleted"<<endl;
+        cout << "All Contact list has been deleted" << endl;
     }
 }
 
+int EditContacts()
+{
+    node *temp = head;
+    cout << "***********" << endl;
+    cout << "  Press 1 if you want to Search By Name" << endl;
+    cout << "  Press 2 if you want to Search By Number" << endl;
+    int Ecommand;
+    cout << "  Enter the Command: ";
+    cin >> Ecommand;
+    string x, y;
+    if (Ecommand == 1)
+    {
+        bool Echeck = false;
+        cout << "  Enter the Name to Edit: ";
+        cin >> x;
+        while (temp != NULL)
+        {
+            if (temp->name == x)
+            {
+                cout << "***********" << endl;
+                cout << "Name: " << temp->name << endl;
+                cout << "Phone Number: " << temp->number << endl;
+                cout << "***********" << endl;
+                Echeck = true;
+                break;
+            }
+            temp = temp->next;
+        }
+        if (Echeck == true)
+        {
+            int command;
+            cout << "  Press 1 to Edit the Contact: ";
+            cin >> command;
+            if (command == 1)
+            {
+                cout << "  Enter New Name: ";
+                cin >> x;
+                cout << "  Enter New Number: ";
+                cin >> y;
+
+                temp->name = x;
+                temp->number = y;
+
+                cout << "  Contact Edited Success Fully" << endl;
+            }
+            else
+            {
+                cout << "  You Enter Wrong Command ... Try Again" << endl;
+            }
+        }
+        else if (Echeck == false)
+        {
+            cout << "  Contact Not Found" << endl;
+        }
+    }
+    else if (Ecommand == 2)
+    {
+        bool Echeck = false;
+        cout << "  Enter the Number to Edit: ";
+        cin >> y;
+        while (temp != NULL)
+        {
+            if (temp->number == y)
+            {
+                cout << "***********" << endl;
+                cout << "Name: " << temp->name << endl;
+                cout << "Phone Number: " << temp->number << endl;
+                cout << "***********" << endl;
+                Echeck = true;
+                break;
+            }
+            temp = temp->next;
+        }
+        if (Echeck == true)
+        {
+            int command;
+            cout << "  Press 1 to Edit the Contact: ";
+            cin >> command;
+            if (command == 1)
+            {
+                cout << "  Enter New Name: ";
+                cin >> x;
+                cout << "  Enter New Number: ";
+                cin >> y;
+
+                temp->name = x;
+                temp->number = y;
+
+                cout << "  Contact Edited Success Fully" << endl;
+            }
+            else
+            {
+                cout << "  You Enter Wrong Command" << endl;
+            }
+        }
+        else if (Echeck == false)
+        {
+            cout << "  There is No Contact of this Number." << endl;
+        }
+    }
+    else
+    {
+        cout << "  You Enter Wrong Command ... Try Again" << endl;
+    }
+}
 
 void menu()
 {
@@ -151,6 +265,7 @@ void menu()
     cout << "Enter 3 to search contact" << endl;
     cout << "Enter 4 to delete contact" << endl;
     cout << "Enter 5 to clear ALL records" << endl;
+    cout << "Enter 6 to Edit Contact" << endl;
 }
 
 int main()
@@ -163,30 +278,34 @@ int main()
         switch (op)
         {
         case 1:
-            cout<<"\n";
+            cout << "\n";
             C_node();
             break;
 
         case 2:
-            cout<<"\n";
-            len=0;
+            cout << "\n";
+            len = 0;
             display();
             break;
 
         case 3:
-            cout<<"\n";
+            cout << "\n";
             search_contact();
             break;
 
         case 4:
-            cout<<"\n";
+            cout << "\n";
             at_given();
             break;
 
         case 5:
             clear_all();
             break;
-            
+
+        case 6:
+            EditContacts();
+            break;
+
         default:
             cout << "Invalid Option" << endl;
         }
